cb3136d884709723c5248f5598c514fab92f609f
scseong <goidtv@naver.com>
2023-04-20 22:56:43 +0900

    feat: Youtube API 응답을 Mocking하여 API 개발 환경을 만든다 (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
+
 root.render(
   <React.StrictMode>
     <App />
