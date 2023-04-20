bc1c71ad5eeadb471d72cf8bdc07f6d860c0f603
scseong <82589401+scseong@users.noreply.github.com>
2023-04-20 23:18:38 +0900

    feat: MSW를 도입해 Mocking하여 API 개발 환경을 만든다.
    * feat: Youtube API 응답을 Mocking하여 API 개발 환경을 만든다 (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function App() {
+  console.log(fetch('popular').then(res => res.json()).then(console.log));
+  console.log(fetch('related').then(res => res.json()).then(console.log));
   return <div>Youtube</div>;
 }
