e7d5521c38b3f6a5e6570080e345e696fc6dee8b
scseong <82589401+scseong@users.noreply.github.com>
2023-04-23 00:24:53 +0900

    feat: 라우터를 설정한다. (#9)
    * docs: Readme 업데이트 및 Issue/PR 템플릿 등록
    
    * feat: 라우터를 설정한다 (#5)

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './fonts.css';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser')
-  worker.start()
+  const { worker } = require('./mocks/browser');
+  worker.start();
 }
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
