1324032918aab1302bcfcb09100b254ea5766abf
scseong <82589401+scseong@users.noreply.github.com>
2023-04-23 00:01:49 +0900

    Revert "feat: 라우터를 설정한다. (#7)"
    This reverts commit 95e37c200c370233464f777c457fe2d43767f838.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
-import Header from './components/Header';
-import { Outlet } from 'react-router-dom';
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  );
+  return <div>App</div>;
 }
+
