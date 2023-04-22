175532778f872e268679cc6a894ebf98e6fda27c
scseong <82589401+scseong@users.noreply.github.com>
2023-04-23 00:19:16 +0900

    Revert "feat: 라우터를 설정한다. (#7)"
    This reverts commit 95e37c200c370233464f777c457fe2d43767f838.

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './fonts.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './Router';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser');
-  worker.start();
-}
-
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
+
