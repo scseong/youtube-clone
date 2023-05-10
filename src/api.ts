9e23b7fe9166436c7aad4aee273b96af3abe4ad1
scseong <goidtv@naver.com>
2023-05-10 16:37:48 +0900

    feat: 실제 Youtube API 사용하여 메인 페이지 영상 목록 출력 및 영상 재생 (#11)

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/videos';
+
 export const fetchPopularVideos = async () => {
-  const response = await axios.get('/popular');
+  const response = await axios.get(
+    `${BASE_URL}?part=snippet&chart=mostPopular&maxResults=24&regionCode=KR&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`,
+  );
   return response.data.items;
 };
